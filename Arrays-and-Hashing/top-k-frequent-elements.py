fb4a7aa40abb5c30b579d53059c7b722f543a20f
Jerrad-Johnson <j_johnson21@mail.fhsu.edu>
2025-01-15 16:09:39 -0700

    top-k ... corrected

--- Arrays-and-Hashing/top-k-frequent-elements.py
+++ Arrays-and-Hashing/top-k-frequent-elements.py
@@ -6,30 +6,40 @@ def topKFrequent(nums, k):
        else:
            map[entry] += 1;
 
-    answers = [];
-
+    correlatedMap = [];
     for entry in map:
-        answers.append([entry, map[entry]]);
-        if len(answers) == k:
-            break;
+        correlatedMap.append([entry, map[entry]]);
 
-    for entry in answers:
-        del(map[entry[0]]);
-
-    for entry in map:
-        currentAnswer = answers[-1];
-        currentMap = map[entry]
-        if currentAnswer[1] < currentMap:
-            answers.append([entry, map[entry]]);
-            answers.pop(0);
-
-    formatted = [];
+    topK = sorted(correlatedMap, key=lambda x: x[1]);
+    answerSet = topK[-k:];
+    return([entry[0] for entry in answerSet]);
 
 
-    for entry in answers:
-        formatted.append(entry[0]);
 
-    return formatted;
+    #
+    # for entry in map:
+    #     answers.append([entry, map[entry]]);
+    #     if len(answers) == k:
+    #         break;
+    # print(answers);
+    #
+    # for entry in answers:
+    #     del(map[entry[0]]);
+    #
+    # for entry in map:
+    #     currentAnswer = answers[-1];
+    #     currentMap = map[entry]
+    #     if currentAnswer[1] < currentMap:
+    #         answers.append([entry, map[entry]]);
+    #         answers.pop(0);
+    #
+    # formatted = [];
 
-nums = [1, 2, 2, 3, 3, 3];
-topKFrequent(nums, 2);
+
+    # for entry in answers:
+    #     formatted.append(entry[0]);
+
+    return ;
+
+nums = [4,1,-1,2,-1,2,3];
+print(topKFrequent(nums, 2));
