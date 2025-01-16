e978ad4e892a5eb1886bff18b9d0964a8391fba4
Jerrad-Johnson <j_johnson21@mail.fhsu.edu>
2025-01-15 20:09:36 -0700

    top-k ... Watched video, attempted concept. Does not pass all tests.

---
+++ Arrays-and-Hashing/top-k-frequent-elements-youtube.py
@@ -0,0 +1,25 @@
+def topKFrequent(nums, k):
+    lengthOfArray = len(nums);
+    answerSet = [];
+    currPos = {};
+
+    for _ in range(lengthOfArray):
+        answerSet.append([]);
+
+    for num in nums:
+        if num not in currPos:
+            currPos[num] = 1;
+        else:
+            currPos[num] += 1;
+
+    for num in currPos:
+        answerSet[currPos[num]].append(num);
+
+    while answerSet and not answerSet[-1]:
+        answerSet.pop();
+
+    flattened = [item for sublist in answerSet for item in sublist];
+    return(flattened[-k:]);
+
+nums = [4,1,-1,2,-1,2,3];
+print(topKFrequent(nums, 2));
